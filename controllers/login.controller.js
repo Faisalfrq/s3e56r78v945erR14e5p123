4f6ff5e0cabbd3600f0a309459f1ae58c052020b
Faisal Farooq <faisalfarooq.edu@gmail.com>
2023-04-18 13:42:04 +0500

    Changes to be committed: 	modified:   controllers/login.controller.js 	modified:   models/user.model.js

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -6,11 +6,7 @@ const config = require('../config/auth.config')
 const bcrypt = require('bcryptjs')
 
 exports.signup = async (req, res) => {
-    //Existing user already
-    //Hashed password
-    //User Creation
-    //toke generation
-    const { name, email, password, address, phone, profileImg, posts } = req.body
+    const { name, email, password, phone, applyAs, DevSub, TrainerSub} = req.body
     try {
         const existingUser = await User.findOne({ email: email })
         if (existingUser.email) {
@@ -19,15 +15,13 @@ exports.signup = async (req, res) => {
                 message: "email already exists"
             })
         }
-        const hashedPassword = await bcrypt.hash(password, 10)
+        //const hashedPassword = await bcrypt.hash(password, 10)
         const result = await db.create({
             name: name,
             email: email,
-            password: hashedPassword,
-            address: address,
+            password: password,
             phone: phone,
-            profileImg: profileImg,
-            posts: posts
+            applyAs: applyAs
         })
 
         const token = jwt.sign({ email: result.email, id: result.id }, config.secret)
@@ -45,71 +39,7 @@ exports.signup = async (req, res) => {
 
 }
 
-// exports.login = (req, res) => {
-//     const { email, password } = req.body;
-//     const userLoginInfo = {
-//         email: email,
-//         password: password
-//     }
 
-//     console.log('entered the login controller', password, email);
-//     if (!email || !password) {
-//         return res.send({
-//             status: "error",
-//             message: "user password or email not provided"
-//         })
-//     }
-//     User.findOne({ password }, (err, user) => {
-//         if (err) {
-//             return res.status(500).send({
-//                 status: "error",
-//                 message: "something went wrong, please try again later"
-//             })
-//         }
-//         console.log('entered the find user call back', user);
-//         if (user) {
-//             console.log('user found')
-//             User.findOne({ email }, (err, email) => {
-//                 console.log('entered the find email call back', email)
-//                 if (err) {
-//                     return res.status(500).send({
-//                         status: "error",
-//                         message: "email not correct"
-//                     })
-//                 }
-//                 if (email) {
-//                     console.log('email found')
-//                     const token = jwt.sign(userLoginInfo, config.secret, { expiresIn: config.tokenLife, })
-//                     const refreshToken = jwt.sign(userLoginInfo, config.refreshTokenSecret, { expiresIn: config.refreshTokenLife })
-//                     const response = {
-//                         user: user.name,
-//                         email: user.email,
-//                         token: token,
-//                         refreshToken: refreshToken,
-//                     }
-//                     tokenList[refreshToken] = response
-//                     return res.status(200).send({
-//                         status: "Successfully logged in",
-//                         data: response
-//                     });
-//                 } else {
-//                     console.log('email not found');
-//                     return res.status(500).send({
-//                         status: "error",
-//                         message: "user password or email not found"
-//                     })
-//                 }
-//             })
-//         } else {
-//             console.log('user not found');
-//             return res.status(500).send({
-//                 status: "error",
-//                 message: "user password or email not found"
-//             })
-//         }
-//     })
-
-// }
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
--- models/user.model.js
+++ models/user.model.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    phone: Number
+    phone: Number,
+    applyAs: String
   });
 
   const User = mongoose.model("User", userSchema);
