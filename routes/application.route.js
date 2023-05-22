07cd32dcde0311df27e927028977cbb5c1b82e83
faisal <faisalfarooq.edu@gmail.com>
2023-05-22 01:26:16 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   routes/application.route.js
    
    Changes not staged for commit:
    	modified:   controllers/application.controller.js
    	modified:   models/index.js
    	modified:   models/user.applications.schema.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   server.js
    
    Untracked files:
    	controllers/cv.controller.js
    	models/cv.schema.js
    	routes/cv.route.js

--- routes/application.route.js
+++ routes/application.route.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const applicationController = require("../controllers/application.controller");
-const multer = require("multer");
 
-// Create the multer middleware
-const upload = multer();
+
 
 // Route for submitting an application
-router.post("/submit-application", upload.fields([{ name: "cv" }, { name: "resume" }]), applicationController.submitApplication);
+router.post("/submitApplication", applicationController.submitApplication);
 
 module.exports = router;
